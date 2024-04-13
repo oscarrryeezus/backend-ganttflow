7519076c668db39929a148cc3ef909504f76662f
ABOK451 <130432973+ABOK451@users.noreply.github.com>
2024-04-13 10:31:18 -0600

    Add files via upload

--- src/routes/empleadoRoutes.js
+++ src/routes/empleadoRoutes.js
@@ -1,24 +1,10 @@
-// empleadoRoutes.js
-
 const express = require('express');
 const router = express.Router();
+
+
 const empleadoController = require('../controllers/empleadoController');
-const multer = require('multer');
-
-// Multer configuration for file upload
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/'); // Directory where uploaded files will be stored
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname); // File name will be timestamp + original name
-    }
-});
-
-const upload = multer({ storage: storage });
 
 router.post('/', empleadoController.create);
-router.post('/cargamasiva', empleadoController.createMany);
 router.get('/', empleadoController.getAll);
 router.get('/:correo', empleadoController.getById);
 router.put('/:correo', empleadoController.update);
