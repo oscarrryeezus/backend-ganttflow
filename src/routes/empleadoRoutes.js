8dabb3ceb5cd552489d7195df8c6763dc95c6275
oscarrryeezus <0scardanielmn20040428@gmail.com>
2024-04-15 21:07:39 -0600

    este es el bueno

--- src/routes/empleadoRoutes.js
+++ src/routes/empleadoRoutes.js
@@ -11,6 +11,6 @@ router.put('/:correo', empleadoController.update);
 router.delete('/:correo', empleadoController.delete);
 router.get('/exists/:correo', empleadoController.verificarCorreoExistente);
 router.put('/actualizarHorario/:correo', empleadoController.updateByEmail);
-router.post('/cargamasiva/')
+router.post('/cargamasiva/', empleadoController.cargaMasiva);
 
 module.exports = router;
